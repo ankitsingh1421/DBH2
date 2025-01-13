9d30c37445518b24672af8b3675a63c2694bb4a9
ankitsingh <ankitsingh75107@gmail.com>
2025-01-13 21:20:35 +0530

    add new files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,15 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,5 +18,8 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'build', // Specify the output directory as 'build'
   },
 });
