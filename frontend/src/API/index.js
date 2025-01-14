75eb6da3668e68adc4e97f91e12c66c00ed21d3f
Ankit Singh <122378526+ankitsingh1421@users.noreply.github.com>
2025-01-14 22:29:11 +0530

    Update index.js

--- frontend/src/API/index.js
+++ frontend/src/API/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000", // Replace with your server URL
+    baseURL: "https://dbh2-2tr8.onrender.com", // Replace with your server URL
 });
 
 export default api;
